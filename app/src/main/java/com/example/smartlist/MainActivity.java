e98b965ac2c89cc1687cc68c4f8cf7d0563206b2
alexis guerra <65685428+alexis-guerra@users.noreply.github.com>
2024-03-19 22:47:04 -0700

    Login de usuarios

--- app/src/main/java/com/example/smartlist/MainActivity.java
+++ app/src/main/java/com/example/smartlist/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.smartlist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.SearchView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements  SearchView.OnQueryTextListener{
 
+    SearchView txtBuscar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtBuscar=findViewById(R.id.txtBuscar);
+
+    txtBuscar.setOnQueryTextListener(this);
+
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        return false;
     }
 }
