0edf98d06b4a6676dbd93d058d7239cc0700ce38
alexis guerra <65685428+alexis-guerra@users.noreply.github.com>
2024-03-22 22:00:20 -0700

    Home Page de carton

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,7 @@ dependencies {
 
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
-    implementation("com.google.android.support:design:29.1.0")
+    implementation("com.google.android.support:design:28.0.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
